fdb0221caa289ccdb24743d59bc926f8847aa8f9
Hajime Nakagami <nakagami@gmail.com>
2015-05-13 13:54:41 +0900

    remove import statement (no use)

--- c2/splitter/janome/janome.py
+++ c2/splitter/janome/janome.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import re
-import unicodedata
 from janome.tokenizer import Tokenizer
 
 from Products.ZCTextIndex.ISplitter import ISplitter
